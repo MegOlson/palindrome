bc619d24782c21226b8a66501ecd14be2fa18fdb
Bably Kumari and Megan Olson <bably@>
2017-09-06 09:57:48 -0700

    pass test to work for integers

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,5 +1,6 @@
 public def palindrome
-    array = self.split("")
+    new_self = self.to_s
+    array = new_self.split("")
     array.each_index do |i|
       if array[i] == array[array.length - i -1]
         return "palindrome"
--- spec/palindrome_spec.rb
+++ spec/palindrome_spec.rb
@@ -5,4 +5,7 @@ describe('#palindrome') do
   it("identifies if a word is a palindrome") do
     expect(("madam").palindrome()).to(eq("palindrome"))
   end
+  it("identifies if an integer is a palindrome") do
+    expect((121).palindrome()).to(eq("palindrome"))
+  end
 end
