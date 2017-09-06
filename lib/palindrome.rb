28fea4cac53824ae246fcead1ee24aa65c075327
Bably Kumari and Megan Olson <bably@>
2017-09-06 09:48:15 -0700

    change logic

--- lib/palindrome.rb
+++ lib/palindrome.rb
@@ -1,7 +1,9 @@
 public def palindrome
-  if self == self.reverse
-    return "palindrome"
-  else
-    return "not a palindrome"
+    array = self.split("")
+    array.each_index do |i|
+      if array[i] == array[array.length - i -1]
+        return "palindrome"
+      else return "not a palindrome"
+    end
   end
 end
